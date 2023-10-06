f5a9ff64b7d71dc4ad11e5b0292592aafa7d589a
Cassia Carvalho <contato.cassia.carvalho@gmail.com>
2023-10-06 17:37:58 -0300

    attexe02

---
+++ Javascript/modulo1/exe02.js
@@ -0,0 +1,9 @@
+let nota1, nota2, nota3, media;
+
+nota1 = parseFloat(prompt("Qual a sua primeira nota? "));
+nota2 = parseFloat(prompt("Qual a sua segunda nota? "));
+nota3 = parseFloat(prompt("Qual a sua terceira nota? "));
+
+media= (nota1 + nota2 + nota3) / 3;
+
+console.log("A sua média é: " + media);
